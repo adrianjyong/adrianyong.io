4e8e2b164be1859c8a60fb8ae4cb0f0ba89b65d4
ajyong <adrianjyong@gmail.com>
2020-10-06 17:50:58 -0400

    Changed page image from placeholder to me

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "pokercat.PNG" }) {
+      placeholderImage: file(relativePath: { eq: "me.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
